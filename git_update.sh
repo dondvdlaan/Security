d4b9f91414eb155717863f48ad312a90d49ea28d
dondvdlaan <dondvdlaan@gmail.com>
2023-09-18 16:42:02 +0200

    Update Readme

--- git_update.sh
+++ git_update.sh
@@ -9,7 +9,7 @@ chmod u+x git_update.sh
 #cd idea-IU-231.8109.175/bin
 
 git add .
-git commit -m "Update on Java2/Spring Security"
+git commit -m "Update Readme"
 #git push Security
 git push --set-upstream Security master
 
